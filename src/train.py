a529bccc98795e8780f17ed4aa0c02a20e6ec531
WooSeok Kim <3suksw@gmail.com>
2024-05-25 18:55:30 +0900

    fix: training setup

--- src/train.py
+++ src/train.py
@@ -1,12 +1,40 @@
 import hydra
+import torch
 
-from model import DQN
+from envs.noma_env import NOMA_Env
+from envs.vector_env import VectorizedEnv
+from model.ann import ANN
+from trainer.trainer import Trainer
 
 
 @hydra.main(version_base=None, config_path="config", config_name="train")
 def train(cfg):
-    """Training code goes here"""
-    model = DQN(input_dim=cfg.input_dim, output_dim=cfg.output_dim)
+    device = "cpu"
+
+    env = NOMA_Env(device)
+    env = VectorizedEnv(env=env, num_envs=cfg.batch_size)
+
+    model = ANN(
+        input_dim=cfg.input_dim,
+        hidden_dim=cfg.hidden_dim,
+        batch_size=cfg.batch_size,
+        num_users=cfg.num_users,
+        num_channels=cfg.num_channels,
+    )
+
+    trainer = Trainer(
+        env=env,
+        model=model,
+        metric=cfg.metric,
+        accelerator="cpu",
+        num_users=cfg.num_users,
+        num_channels=cfg.num_channels,
+        num_episodes=cfg.num_episodes,
+        num_tests=cfg.num_tests,
+        loss_threshold=cfg.loss_threshold,
+    )
+
+    trainer.train()
 
 
 if __name__ == "__main__":
