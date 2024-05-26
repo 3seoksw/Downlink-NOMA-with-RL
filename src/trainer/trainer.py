c94d3472da757f9bb3a2a404b8b91577b37e8b58
WooSeok Kim <3suksw@gmail.com>
2024-05-26 22:47:08 +0900

    feat: `fit()` for substitute `train()`

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -5,6 +5,7 @@ import numpy as np
 from envs.core_env import BaseEnv
 from model.base_model import BaseModel
 from trainer.logger import Logger
+from model.ann import ANN
 from tqdm import tqdm
 
 
@@ -23,11 +24,14 @@ class Trainer:
         loss_threshold: float = 1e-3,
         save_dir: str = "simulations",
         save_every: int = 10,
+        method: str = "Q-Learning",
     ):
         """
         Creates two sets of training-purpose objects: baseline objects and testing objects.
         Each comprises `env` and `model`.
         """
+        self.method = method
+
         self.N = num_users
         self.K = num_channels
 
@@ -46,6 +50,7 @@ class Trainer:
         self.loss_threshold = loss_threshold
 
         self.optimizer = torch.optim.Adam(self.model.parameters())
+        self.loss_func = torch.nn.MSELoss()
 
         if accelerator not in ["cpu", "mps", "gpu"]:
             raise Exception("`accelerator` should be either 'cpu', 'mps', or 'gpu'.")
@@ -63,6 +68,46 @@ class Trainer:
             self.device = torch.device("cpu")
 
         self.model.to(self.device)
+
+        self.online_model = ANN()
+        self.target_model = ANN().load_state_dict(self.online_model.state_dict())
+
+    def fit(self):
+        for episode in range(self.num_episodes):
+            state, _ = self.env.reset()
+            loss_reward = torch.zeros(state.shape[0], 1)
+            self.model._reset()
+
+            pred_rewards = []
+            steps = tqdm(
+                range(self.N - 1),
+                desc="Testing model",
+                bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+            )
+            for _ in steps:
+                out = self.model(state)
+                pred_reward, action = torch.max(out, dim=1)
+                pred_rewards.append(pred_reward)
+
+                next_state, reward, _, _ = self.env.step(action)
+                state = next_state
+
+            loss_reward = reward.float()
+            loss_reward.to(self.device)
+
+            pred_rewards = torch.stack(pred_rewards)
+            if self.metric == "MSR":
+                pred_loss_reward = torch.sum(pred_rewards, dim=0)
+            elif self.metric == "MMR":
+                pred_loss_reward = torch.min(pred_rewards, dim=0)
+            else:
+                raise KeyError()
+
+            loss = self.loss_func(pred_loss_reward, loss_reward)
+            print(episode, loss)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
     def train(self):
         T_s = 5  # training stopping criterion
