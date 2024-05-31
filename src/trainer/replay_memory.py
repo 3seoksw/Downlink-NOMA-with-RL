14a15109976c4f6b9e6963bb57ee1c556dc3efc2
WooSeok Kim <3suksw@gmail.com>
2024-05-31 17:27:33 +0900

    !fix: add next_state

--- src/trainer/replay_memory.py
+++ src/trainer/replay_memory.py
@@ -9,21 +9,24 @@ class ReplayMemory:
         self.buffer = deque(maxlen=10000)
         self.batch_size = batch_size
 
-    def save_into_memory(self, prev_state, state, action, reward):
+    def save_into_memory(self, prev_state, state, next_state, action, done, reward):
         prev_state = prev_state.squeeze(0)
         state = state.squeeze(0)
+        next_state = next_state.squeeze(0)
         
         self.buffer.append((
             prev_state,
             state,
+            next_state,
             action,
+            done,
             reward,
         ))
 
     def sample_from_memory(self):
         batch = random.sample(self.buffer, self.batch_size)
-        state, next_state, action, reward = map(torch.stack, zip(*batch))
-        return state, next_state, action, reward
+        prev_state, state, next_state, action, done, reward = map(torch.stack, zip(*batch))
+        return prev_state, state, next_state, action, done, reward
 
     def get_buffer(self):
         return self.buffer
