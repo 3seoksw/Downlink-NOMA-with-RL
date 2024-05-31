090c580fa65158d484fcf285dcf306ba0cc1cbba
WooSeok Kim <3suksw@gmail.com>
2024-05-31 23:12:57 +0900

    fix: remove `prev_state` from the memory

--- src/trainer/replay_memory.py
+++ src/trainer/replay_memory.py
@@ -9,24 +9,24 @@ class ReplayMemory:
         self.buffer = deque(maxlen=10000)
         self.batch_size = batch_size
 
-    def save_into_memory(self, prev_state, state, next_state, action, done, reward):
-        prev_state = prev_state.squeeze(0)
+    def save_into_memory(self, state, next_state, action, done, reward):
         state = state.squeeze(0)
         next_state = next_state.squeeze(0)
-        
-        self.buffer.append((
-            prev_state,
-            state,
-            next_state,
-            action,
-            done,
-            reward,
-        ))
+
+        self.buffer.append(
+            (
+                state,
+                next_state,
+                action,
+                done,
+                reward,
+            )
+        )
 
     def sample_from_memory(self):
         batch = random.sample(self.buffer, self.batch_size)
-        prev_state, state, next_state, action, done, reward = map(torch.stack, zip(*batch))
-        return prev_state, state, next_state, action, done, reward
+        state, next_state, action, done, reward = map(torch.stack, zip(*batch))
+        return state, next_state, action, done, reward
 
     def get_buffer(self):
         return self.buffer
