b9ab11f42571e8deca8104ba1093e9f5eba0460b
Karim <51705815+2jeonghoon@users.noreply.github.com>
2024-06-14 15:34:59 +0900

    Feat: Constrants Class

---
+++ src/util/constants.py
@@ -0,0 +1,48 @@
+class Constants:
+    @property
+    def n_steps(self) -> int:
+        return 0
+    
+    @property
+    def usr_idx_history(self) -> int:
+        return 1
+    
+    @property
+    def user_info(self) -> int:
+        return 2
+
+    @property
+    def user_idx(self) -> int:
+        return 0
+    
+    @property
+    def x(self) -> int:
+        return 1
+    
+    @property
+    def y(self) -> int:
+        return 2
+    
+    @property
+    def distance(self) -> int:
+        return 3
+    
+    @property
+    def power(self) -> int:
+        return 4
+    
+    @property
+    def data_rate(self) -> int:
+        return 5
+        
+    @property
+    def CNR(self) -> int:
+        return 6
+    
+    @property
+    def history_idx(self) -> int:
+        return 7
+    
+    @property
+    def channel(self) -> int:
+        return 8
